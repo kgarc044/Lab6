3c26243348182c54f0e73dc6249916e61a2111f2
Kyle Gabriel Garcia <kgarc044@wch136-24.cs.ucr.edu>
2020-01-28 00:09:11 -0800

    typed out main.c

---
+++ Lab6_SynchSMS/source/main.c
@@ -0,0 +1,63 @@
+/*	Author: kgarc044
+ *  Partner(s) Name: RICHARD BRAIN
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |- 0x80;
+}
+
+void TimerOff() {
+	TCCR1B = 0x00;
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) {
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+void TimerSet(unsigned long M) {
+	_avr_time_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+
+
+void main() {
+	DDRB = 0xFF;
+	PORTB = 0x00;
+	TimerSet(1000);
+	TimerOn();
+	unsigned char tmpB = 0x00;
+	while(1) {
+		TimerFlag = 0;
+	}
+}
+
+
+
+
